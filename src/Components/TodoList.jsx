f9c9a0ca600c1fd097248a908c388683142ed7b1
V3lvvy <4rwanubar@gmail.com>
2025-05-18 12:47:20 -0400

    Completed Todo List

--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -13,6 +13,25 @@ const TodoList = () => {
         }
     };
 
+    const handleAddList = (index) => {
+        if (listInputs[index] && listInputs[index].trim() !== '') {
+            const newTodos = [...todos];
+            newTodos[index].lists.push(listInputs[index]);
+            setTodos(newTodos);
+            setListInputs({...listInputs, [index]: ''});
+        }
+    };
+
+    const handleListInputChange = (index, value) => {
+        setListInputs({...listInputs, [index]: value });
+    };
+
+    const handleDeleteTodo = (index) => {
+        const newTodos = [...todos];
+        newTodos.splice(index, 1);
+        setTodos(newTodos);
+    };
+    
   return (
     <>
       <div className="todo-container">
@@ -34,6 +53,22 @@ const TodoList = () => {
                 <div className='heading_todo'>
                     <h3>{todo.heading}</h3> 
                     <button className='delete-button-heading' onClick={() => handleDeleteTodo(index)}>Delete heading</button>
+                </div>
+                <ul>
+                    {todo.lists.map((list, listIndex) => (
+                        <li key={listIndex} className='todo_inside_list'>
+                            <p>{list}</p>
+                        </li>
+                    ))}
+                </ul>
+                <div className='add_list'>
+                    <input
+                    type="text"
+                    className='list-input'
+                    placeholder='Add List'
+                    value={listInputs[index] || ''}
+                    onChange={(e) => handleListInputChange(index, e.target.value)}/>
+                    <button className='add-list-button' onClick={() => handleAddList(index)}>Add List</button>
                 </div>
             </div>
         ))}
